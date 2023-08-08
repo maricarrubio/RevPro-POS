f13daa5abe4643ba19e7ac943e25f1a323e843cf
Maricar Rubio <maricarlaquianrubio02@gmail.com>
2023-08-08 15:41:17 +0800

    added state management to login and register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './style.css'
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <>
       <div className='login template d-flex justify-content-center align-items-center vh-100 bg-primary'>
         <div className='form_container p-5 rounded bg-white'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h3 className='text-center'> Sign In</h3>
             <div className='mb-2'>
               <label htmlFor="email">Employee/Admin ID</label>
-              <input type="ea_id" placeholder='Enter Employee/Admin ID' className='form-control'/>
+              <input type="ea_id" placeholder='Enter Employee/Admin ID' className='form-control' value={email} onChange={(event) => setEmail(event.target.value)} />
             </div>
             <div className='mb-2'>
               <label htmlFor="password">Password</label>
-              <input type="password" placeholder='Enter Password' className='form-control'/>
+              <input type="password" placeholder='Enter Password' className='form-control' value={password} onChange={(event) => setPassword(event.target.value)} />
             </div>
             <div className='mb-2'>
               <input type="checkbox" className='custom-control custom-check' id='check' />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 function Register() {
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, id, password, confirmPassword);
+  };
+
   return (
     <div className='register template d-flex justify-content-center align-items-center vh-100 bg-primary'>
     <div className='form_container p-5 rounded bg-white'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3 className='text-center'>Register</h3>
         <div className='mb-2'>
           <label htmlFor="ename">Employee/ Admin Name</label>
-          <input type="text" placeholder='Enter Employee/ Admin Name' className='form-control'/>
+          <input type="text" placeholder='Enter Employee/ Admin Name' className='form-control' value={name} onChange={(event) => setName(event.target.value)} />
         </div>
         <div className='mb-2'>
           <label htmlFor="eID">Employee/ Admin ID</label>
-          <input type="text" placeholder='Enter Employee/ Admin ID' className='form-control'/>
+          <input type="text" placeholder='Enter Employee/ Admin ID' className='form-control' value={id} onChange={(event) => setId(event.target.value)} />
         </div>
         <div className='mb-2'>
           <label htmlFor="password">Password</label>
-          <input type="password" placeholder='Enter Password' className='form-control'/>
+          <input type="password" placeholder='Enter Password' className='form-control' value={password} onChange={(event) => setPassword(event.target.value)}/>
         </div>
         <div className='mb-2'>
           <label htmlFor="password"> Confirm Password</label>
-          <input type="password" placeholder='Enter Password' className='form-control'/>
+          <input type="password" placeholder='Enter Password' className='form-control' value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)}/>
         </div>
         <div className='d-grid'>
             <button className='btn btn-primary'>Register</button>
@@ -37,6 +47,6 @@ function Register() {
     </div>
   </div>
   )
-}
+};
 
 export default Register
